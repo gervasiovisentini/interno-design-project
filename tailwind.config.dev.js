c611ef468e16b3f0281426aee937d05cea23ab7b
gervapty <visentini.gervasio@gmail.com>
2024-02-05 19:18:19 -0300

    listo, falta solucionar error

--- tailwind.config.dev.js
+++ tailwind.config.dev.js
@@ -1,41 +1,42 @@
+"use strict";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
-    container:{
-      padding:{
-        DEFAULT: '15px',
+    container: {
+      padding: {
+        DEFAULT: '15px'
       }
     },
-    screens:{
+    screens: {
       sm: '640px',
       md: '768px',
       lg: '960px',
-      xl:'1200px',
+      xl: '1200px'
     },
-    fontFamily:{
+    fontFamily: {
       primary: 'DM Serif Display',
-      secondary: 'Jost',
+      secondary: 'Jost'
     },
-    backgroundImage:{
+    backgroundImage: {
       hero: 'url(/assets/hero/bg.jpg)',
-      grid: 'url(/assets/grid.png)',
+      grid: 'url(/assets/grid.png)'
     },
     extend: {
-      colors:{
-        primary:{
+      colors: {
+        primary: {
           DEFAULT: '#292f36',
-          hover:'#343e4a',
+          hover: '#343e4a'
         },
         secondary: '#4d5053',
         accent: {
           DEFAULT: '#cda274',
           secondary: 'f4f0ec',
-          hover: '#b88c5d',
-        },
-      },
-    },
+          hover: '#b88c5d'
+        }
+      }
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
